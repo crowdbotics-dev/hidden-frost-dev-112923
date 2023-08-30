a7bdf46492d75eeedfb9e22458bc315fd9a8f3eb
Crowdbotics <engineering@crowdbotics.com>
2023-08-30 10:24:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-frost-dev-112923",
+  url: "https://hidden-frost-dev-112923.botics.co",
+  api: "https://hidden-frost-dev-112923.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
